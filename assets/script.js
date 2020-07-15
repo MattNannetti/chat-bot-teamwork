8f6f1f8a8195e39d9a583d7993b4e3256f3f4199
unknown <noemie.uylenbroeck@gmail.com>
2020-07-15 11:48:48 +0200

    ajouts sur js et html

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,26 @@
-let messagesBienvenue = ["Bonjour", "Salut", "Coucou"];
+let messagesBienvenue = ["Hello !", "Hi !", "What's up ?"];
 let messageBienvenueRandom = messagesBienvenue[Math.floor(Math.random() * messagesBienvenue.length)];
 
-document.getElementById("!!!!!!!").innerHTML = messageBienvenueRandom;
-document.getElementById("!!!!!!!").innerHTML = "Are you feeling well today ?";
+document.getElementById("botoutput").innerHTML = messageBienvenueRandom + " Are you feeling well today?";
 
-let inputstring = "renard computer, écran";
-if (inputstring.search("yes") && !inputstring.search("no")) {
-    let messagesGood = ["Cool ta vie", "Content pour toi", "Va dormir"];
-    let messageGoodRandom = messagesGood[Math.floor(Math.random() * messagesGood.length)];
-    console.log(messageGoodRandom);
-} else if (inputstring.search("no") && !inputstring.search("no")) {
-    let messagesBad = ["Je m'en fous", "Déso pour toi", "Raconte pas ta vie"];
-    let messageBadRandom = messagesBad[Math.floor(Math.random() * messagesBad.length)];
-    console.log(messageBadRandom);
-} else {
-    for (let i = 0; i <= 3;) {
-        let messageIncompréhension = "Pas compris";
-        console.log(messageIncompréhension);
-    } let messageAgressif = "Ecris mieux";
-    console.log(messageAgressif);
-}
+document.getElementById("submit").addEventListener("click", () => {
+    let stringAnswer = document.getElementById("answer").value;
+    
+    if (stringAnswer.search("yes")) {
+        let messagesGood = ["Cool ta vie", "Content pour toi", "Va dormir"];
+        let messageGoodRandom = messagesGood[Math.floor(Math.random() * messagesGood.length)];
+        console.log(messageGoodRandom);
+
+    } else if (stringAnswer.search("no")) {
+        let messagesBad = ["Je m'en fous", "Déso pour toi", "Raconte pas ta vie"];
+        let messageBadRandom = messagesBad[Math.floor(Math.random() * messagesBad.length)];
+        console.log(messageBadRandom);
+
+    } else {
+        for (let i = 0; i <= 3; i++) {
+            let messageIncompréhension = "Pas compris";
+            console.log(messageIncompréhension);
+        } let messageAgressif = "Ecris mieux";
+        console.log(messageAgressif);
+    }
+});
