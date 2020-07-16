c760aa9931fdb7dacf1bc3e569c13751b80886d5
unknown <noemie.uylenbroeck@gmail.com>
2020-07-16 09:43:38 +0200

    ajout shutdown

--- assets/script.js
+++ assets/script.js
@@ -10,39 +10,65 @@ document.getElementById("submit").addEventListener("click", function () {
 
 document.getElementById("answer").addEventListener("keyup", function (event) {
     if (event.keyCode === 13) {
-        event.preventDefault();
-        answerBot()
+        answerBot();
     }
 });
 
-function answerBot(){
-    let stringAnswer = document.getElementById("answer").value;
-    if ((stringAnswer.toLowerCase().includes("yes")) && (stringAnswer.toLowerCase().includes("no"))) {
-        let messageMitigé = "Choose yes or no?";
-        document.getElementById("botoutput").innerHTML = messageMitigé;
-    }
-    else if (stringAnswer.toLowerCase().includes("yes")) {
-        let messagesGood = ["Cool story bro", "Urgh... great..", "Noice"];
-        let messageGoodRandom = messagesGood[Math.floor(Math.random() * messagesGood.length)];
-        document.getElementById("botoutput").innerHTML = messageGoodRandom;
-        setTimeout(function () {
-            let messageFin = "Bye";
-            let buttonDisabled = document.getElementById("submit");
-            buttonDisabled.disabled = true;
-            document.getElementById("botoutput").innerHTML = messageFin;
-        }, 3000);
+function answerBot() {
+    document.getElementById("submit").addEventListener("click", () => {
+        let stringAnswer = document.getElementById("answer").value;
+        if ((stringAnswer.toLowerCase().includes("yes")) && (stringAnswer.toLowerCase().includes("no"))) {
+            let messageMitigé = "Choose yes or no?";
+            document.getElementById("botoutput").innerHTML = messageMitigé;
 
-    } else if (stringAnswer.toLowerCase().includes("no")) {
-        let messagesBad = ["Not interested", "I don't care, sorry", "Booooooooring"];
-        let messageBadRandom = messagesBad[Math.floor(Math.random() * messagesBad.length)];
-        document.getElementById("botoutput").innerHTML = messageBadRandom;
+        } else if (stringAnswer.toLowerCase().includes("yes")) {
+            let messagesGood = ["Cool story bro", "Urgh... great..", "Noice"];
+            let messageGoodRandom = messagesGood[Math.floor(Math.random() * messagesGood.length)];
+            document.getElementById("botoutput").innerHTML = messageGoodRandom;
+            alertRobot();
 
-    } else if (i < 3){
-        i++;
-        let messageIncompréhension = "I didn't get that..";
-        document.getElementById("botoutput").innerHTML = messageIncompréhension;
-    } else{
+        } else if (stringAnswer.toLowerCase().includes("no")) {
+            let messagesBad = ["Not interested", "I don't care, sorry", "Booooooooring"];
+            let messageBadRandom = messagesBad[Math.floor(Math.random() * messagesBad.length)];
+            document.getElementById("botoutput").innerHTML = messageBadRandom;
+            shutDown();
+
+
+        } else if (i < 3) {
+            i++;
+            let messageIncompréhension = "I didn't get that..";
+            document.getElementById("botoutput").innerHTML = messageIncompréhension;
+            shutDown();
+
+        } else {
             let messageAgressif = "Did you spell that right?";
             document.getElementById("botoutput").innerHTML = messageAgressif;
+            shutDown();
         }
+    });
+}
+
+function shutDown(){
+        let alertRobotMessage="attention batterie faible";
+        let countdown1="3";
+        let countdown2="2";
+        let countdown3="1";
+        let messageFin = "Bye";
+        let buttonDisabled = document.getElementById("submit");
+        setTimeout(function(){
+            document.getElementById("botoutput").innerHTML = alertRobotMessage;
+        },2000);
+        setTimeout(function(){
+            document.getElementById("botoutput").innerHTML = countdown1;
+        },4000);
+        setTimeout(function(){
+            document.getElementById("botoutput").innerHTML = countdown2;
+        },5000);
+        setTimeout(function(){
+            document.getElementById("botoutput").innerHTML = countdown3;
+        },6000);
+        setTimeout(function(){ 
+            buttonDisabled.disabled = true;
+            document.getElementById("botoutput").innerHTML = messageFin;
+        },7000);  
 }
